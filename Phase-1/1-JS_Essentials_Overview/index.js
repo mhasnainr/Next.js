4dc55625d2799fe2853f74bb179e2d6e0deed8a8
Hasnain <mhasnainr.415@gmail.com>
2024-12-16 20:50:01 +0500

    Day-1.1: functions

---
+++ Phase-1/1-JS_Essentials_Overview/index.js
@@ -0,0 +1,70 @@
+// -------------------------------------------------------------------------------------------- functions
+
+// types of functions
+
+// 1- declaration
+
+// function greet(name) {
+//   return `Welcome, ${name}!`;
+// }
+
+// console.log(greet("Hasnain"));
+
+// 2- expression
+
+// const greet = function (name) {
+//   return `Welcome, ${name}!`;
+// };
+
+// console.log(greet("Hasnain"));
+
+// 3- Arrow
+
+// const greet = (name) => `Welcome, ${name}!`;
+
+// console.log(greet("Hasnain"));
+
+// -----------------------  Practise task-1
+
+// function sqr(num) {
+//   return num * num;
+// }
+
+// console.log(sqr(6));
+
+// ----------------------- Challenge 1: function-based calculator
+
+// const calculator = (n1, n2, operation) => {
+//   if (operation === "add") {
+//     return n1 + n2;
+//   } else if (operation === "subt") {
+//     return n1 - n2;
+//   } else if (operation === "mult") {
+//     return n1 * n2;
+//   } else if (operation === "dvd") {
+//     return n1 / n2;
+//   } else {
+//     return "Invalid Operation";
+//   }
+// };
+
+// console.log(calculator(12, 4, "add"));
+
+// const operation = (n1, n2) => {
+//   return n1 * n2;
+// };
+
+// console.log(operation(2, 3));
+
+// const msg = (name) => {
+//   console.log(`Welcome ${name}`);
+// };
+// msg("Hasnain");
+
+//
+
+// -------------------------------------------------------------------------------------------- Loops
+
+// ------ for loop:
+
+
